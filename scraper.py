7afba7f8d8bed7b46edad4bb9dfd4888f98cb309
Beeeeeeenjit4 <benjamin.rbascunan@gmail.com>
2026-06-18 00:22:50 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -68,7 +68,7 @@ def scrape_keyword(pagina, keyword, vistos, resultados):
         print(f"  ⚠️ No cargó: {e}")
         return
 
-    for _ in range(1):
+    for _ in range(13):
         pagina.mouse.wheel(0, 3000)
         animacion_carga(pagina, 0.7, "Cargando productos")
 
